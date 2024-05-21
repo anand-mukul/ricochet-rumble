dda95b5f08041428c0f44bb7f6cf56b592f34478
Mukul Anand <mukulanand.biz@gmail.com>
2024-05-21 20:35:33 +0530

    minor fixes before restarting

---
+++ play/js/pieces.js
@@ -0,0 +1,29 @@
+const titan = {
+  image: {
+    src: "assets/pieces/titan.svg",
+  },
+};
+
+const tank = {
+  image: {
+    src: "assets/pieces/tank.svg",
+  },
+};
+
+const semirichohet = {
+  image: {
+    src: "assets/pieces/semi-richohet.svg",
+  },
+};
+
+const richohet = {
+  image: {
+    src: "assets/pieces/richohet.svg",
+  },
+};
+
+const canon = {
+  image: {
+    src: "assets/pieces/canon.svg",
+  },
+};
