4c62b58af374917e542b7daa3032f341a7e36eec
Mukul Anand <mukulanand.biz@gmail.com>
2024-06-02 20:38:33 +0530

    adding mode as per button

--- play/rumble.js
+++ play/rumble.js
@@ -1,24 +1,16 @@
-console.log("I am rumble.js");
+document.addEventListener("DOMContentLoaded", () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const mode = urlParams.get('mode');
 
-document.addEventListener("DOMContentLoaded", function() {
-    var urlParams = new URLSearchParams(window.location.search);
-    var mode = urlParams.get('mode');
-
-    if (mode !== null && (mode === "friend" || mode === "bot")) {
+    if (mode && (mode === "friend" || mode === "bot")) {
         loadScript(mode === "friend" ? "/play/js/withFriend.js" : "/play/js/withBot.js");
     } else {
-        console.error("Invalid or missing mode parameter in URL.");
-        window.location.href = "/play/invalidMode.html"; 
+        window.location.href = "/play/invalidMode.html";
     }
 
     function loadScript(scriptName) {
-        var script = document.createElement("script");
+        const script = document.createElement("script");
         script.src = scriptName;
-        script.onload = function() {
-        };
-        script.onerror = function() {
-            console.error("Failed to load! Please try again.");
-        };
         document.body.appendChild(script);
     }
 });
