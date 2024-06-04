960568c43ca31da4fafeb3f4bc2facd7db69e02f
Mukul Anand <mukulanand.biz@gmail.com>
2024-06-04 06:13:20 +0530

    Homepage Responsiveness added

--- script.js
+++ script.js
@@ -1,48 +1,71 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navBottom = document.getElementById("nav-bottom");
+
+hamburgerMenu.addEventListener("click", function () {
+  navBottom.classList.toggle("active");
+});
+
+hamburgerMenu.addEventListener("touchstart", function () {
+  navBottom.classList.toggle("active");
+});
+
 function aboutAnimination() {
   var aboutRightListAll = document.querySelectorAll(".about-right-list");
   aboutRightListAll.forEach(function (hover) {
-    hover.addEventListener("mouseenter", function () {
-      gsap.to(hover.childNodes[3], {
-        opacity: 1,
-        scale: 1,
-      });
-    });
-    hover.addEventListener("mouseleave", function () {
-      gsap.to(hover.childNodes[3], {
-        opacity: 0,
-        scale: 0,
-      });
-    });
-    hover.addEventListener("mousemove", function (dets) {
-      gsap.to(hover.childNodes[3], {
-        x: dets.x - hover.getBoundingClientRect().x - 50,
-        y: dets.y - hover.getBoundingClientRect().y - 85,
-        duration: 0.5,
-        ease: "power2.out",
-      });
-    });
+    hover.addEventListener("mouseenter", handleHover);
+    hover.addEventListener("mouseleave", handleLeave);
+    hover.addEventListener("mousemove", handleMove);
+
+    hover.addEventListener("touchstart", handleHover);
+    hover.addEventListener("touchend", handleLeave);
+    hover.addEventListener("touchmove", handleMove);
   });
+
+  function handleHover() {
+    gsap.to(this.childNodes[3], { opacity: 1, scale: 1 });
+  }
+
+  function handleLeave() {
+    gsap.to(this.childNodes[3], { opacity: 0, scale: 0 });
+  }
+
+  function handleMove(dets) {
+    var touch = dets.touches ? dets.touches[0] : dets;
+    gsap.to(this.childNodes[3], {
+      x: touch.clientX - this.getBoundingClientRect().x - 50,
+      y: touch.clientY - this.getBoundingClientRect().y - 85,
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  }
 }
 
 function rulesVideo() {
   var rulesCenter = document.querySelector("#rules-video-center");
   var video = document.querySelector("#rules video");
-  rulesCenter.addEventListener("click", function () {
+
+  function playVideo() {
     video.play();
     gsap.to(video, {
       transform: "scaleX(1) scaleY(1)",
       opacity: 1,
       borderRadius: 0,
     });
-  });
-  video.addEventListener("click", function () {
+  }
+
+  function pauseVideo() {
     video.pause();
     gsap.to(video, {
       transform: "scaleX(0.7) scaleY(0)",
       opacity: 0,
       borderRadius: "30px",
     });
-  });
+  }
+
+  rulesCenter.addEventListener("click", playVideo);
+  rulesCenter.addEventListener("touchstart", playVideo);
+  video.addEventListener("click", pauseVideo);
+  video.addEventListener("touchstart", pauseVideo);
 }
 
 aboutAnimination();
